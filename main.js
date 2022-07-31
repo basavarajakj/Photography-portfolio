0aa88bddb547bc1ecc60ebd97545e120b6c085e2
Basavaraja K J <97227305+Basavarajakjbasuu@users.noreply.github.com>
2022-07-31 23:07:12 +0530

    commit

--- main.js
+++ main.js
@@ -19,3 +19,48 @@ textButtons.forEach((textButton) => {
     )
     .join("");
 });
+
+// SWIPER JS SLIDER
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    599: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1023: {
+      slidesPerView: 3,
+      spaceBetween: 60,
+    },
+  },
+});
+
+// RESPNOSIVE NAVBAR
+const nav = document.querySelector(".nav__links");
+const openNavBtn = document.querySelector(".nav__toggle-btn");
+const closeNavBtn = document.querySelector("#nav__toggle-close");
+
+const openNav = () => {
+  nav.style.display = "flex";
+  openNavBtn.style.display = "none";
+  closeNavBtn.style.display = "inline-block";
+};
+
+openNavBtn.addEventListener("click", openNav);
+
+const closeNav = () => {
+  nav.style.display = "none";
+  openNavBtn.style.display = "inline-block";
+  closeNavBtn.style.display = "none";
+};
+
+closeNavBtn.addEventListener("click", closeNav);
+
+nav.querySelectorAll("li a").forEach((navLink) => {
+  navLink.addEventListener("click", closeNav);
+});
