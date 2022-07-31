66e089b12054b310c7a93183b77de150615ff9f5
Basavaraja K J <97227305+Basavarajakjbasuu@users.noreply.github.com>
2022-07-31 18:50:18 +0530

    commit

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 // change nav style on scroll
 window.addEventListener("scroll", () => {
-  document.querySelector("nav").classList.toggle("window-scrolled"),
-    windwo.scrolly > 0;
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scrolled", window.scrollY > 0);
+});
+
+// CONTACT BUTTON(circular text)
+const textButtons = document.querySelectorAll(".contact__btn");
+
+textButtons.forEach((textButton) => {
+  let text = textButton.querySelector("p");
+
+  text.innerHTML = text.innerHTML
+    .split("")
+    .map(
+      (character, index) =>
+        `<span style="transform: rotate(${index * 12}deg)">${character}</span>`
+    )
+    .join("");
 });
